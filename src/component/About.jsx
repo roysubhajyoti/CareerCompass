2df98396ce109c8a10e8e3e7663f71d11d0d74b1
roysubhajyoti <roysubbhajyoti272@gmail.com>
2024-03-05 00:24:06 +0530

    about struct,plan motion,

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,3 +1,36 @@
+import { motion } from "framer-motion";
 export const About = () => {
-  return <div className="bg-gray-700">this is About</div>;
+  return (
+    <div className="min-h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2]  flex justify-center ">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <motion.div className=" text-white font-poppins  flex flex-col absolute top-[10rem]">
+        <motion.h1
+          className="text-5xl font-bold  border-b-2 border-orange-200 mb-[2rem] flex justify-center"
+          initial={{ opacity: 0, x: "100vh" }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{
+            delay: 0.5,
+            duration: 1,
+            type: "spring",
+            stiffness: 110,
+          }}
+        >
+          About Us Career<span className="text-orange-200">Compass</span>
+        </motion.h1>
+        <motion.h2
+          className="text-3xl font-poppins"
+          initial={{ opacity: 0, y: "100vh" }}
+          animate={{ opacity: 1, y: 20 }}
+          transition={{
+            delay: 0.5,
+            duration: 1,
+            type: "tween",
+            stiffness: 110,
+          }}
+        >
+          Objectives <br /> baki Rohit And Ankur likhbe akhane......
+        </motion.h2>
+      </motion.div>
+    </div>
+  );
 };
--- src/component/Plan.jsx
+++ src/component/Plan.jsx
@@ -1,15 +1,31 @@
 import { ThreeDCardDemo } from "./ThreeDCard";
 import student from "../assets/student.avif";
 import parent from "../assets/parent.jpg";
+import { motion } from "framer-motion";
 export const Plan = () => {
   return (
     <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center text-white">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="flex flex-col justify-center items-center absolute top-28">
-        <h1 className="text-4xl">
+        <motion.h1
+          className="text-4xl"
+          initial={{ opacity: 0, x: "100vh" }}
+          animate={{ opacity: 1, x: 20 }}
+          transition={{
+            delay: 0.5,
+            duration: 1,
+            type: "spring",
+            stiffness: 110,
+          }}
+        >
           ....Here You Will Explore all The Paths....
-        </h1>
-        <div className="flex justify-center items-center gap-20">
+        </motion.h1>
+        <motion.div
+          className="flex justify-center items-center gap-20"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.5, transition: 1 }}
+        >
           <ThreeDCardDemo
             section={"The Parents Section"}
             desc={
@@ -26,7 +42,7 @@ export const Plan = () => {
             linkc={"/plans"}
             pic={student}
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
