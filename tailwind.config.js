05db5444b583699bdf8d22225d58d7e7f87c3b76
SoumojitDebnath <soumojitdebnath52@gmail.com>
2024-03-04 14:22:57 +0530

    Login Page done.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      background: {
+        'hero': "url('C:\Users\HP\OneDrive\Desktop\New folder\CareerCompass\src\assets\background-login.mp4')",
+      },
+    },
   },
   plugins: [],
 };
